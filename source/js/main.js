b4f1caf1c51c35cc1aeea2487f920bf9b87532a8
Ulia Usalko <usalko.ulia@gmail.com>
2018-10-14 22:21:15 +0300

    global work

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,9 @@
-console.log('ololo');
+var button = document.querySelector(".navigation__button");
+var navigation = document.querySelector(".navigation");
+var navigation_list = document.querySelector(".navigation__list");
+
+button.addEventListener("click", function() {
+  button.classList.toggle("navigation__button--active");
+  navigation_list.classList.toggle("navigation__list--active");
+  navigation.classList.toggle("navigation--active");
+});
