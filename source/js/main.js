52a8f28361229840b34f4ffeb8629d334e19536a
Ulia Usalko <usalko.ulia@gmail.com>
2018-10-20 22:22:26 +0300

    Работа над доступностью

--- source/js/main.js
+++ source/js/main.js
@@ -7,3 +7,18 @@ button.addEventListener("click", function() {
   navigation_list.classList.toggle("navigation__list--active");
   navigation.classList.toggle("navigation--active");
 });
+
+var srcsets = document.querySelectorAll("[data-srcset]");
+var srcs = document.querySelectorAll("[data-src]");
+
+Array.from(srcsets).forEach(function(srcset) {
+  var value = srcset.getAttribute("data-srcset");
+  srcset.setAttribute("srcset", value);
+  srcset.removeAttribute("data-srcset");
+});
+
+Array.from(srcs).forEach(function(src) {
+  var value = src.getAttribute("data-src");
+  src.setAttribute("src", value);
+  src.removeAttribute("data-src");
+});
